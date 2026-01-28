cc6572b66a82be2cf93ae63af31f21089aa056bb
Brandon Whitman-Orlin <brandonwhitmanorlin@gmail.com>
2026-01-28 03:15:02 -0500

    Update productImages.js

--- src/data/productImages.js
+++ src/data/productImages.js
@@ -1,7 +1,9 @@
 import BeauPin from "../../assets/productimages/characterbuttons/Beau_Pin.png";
+import CarltonPin from "../../assets/productimages/characterbuttons/Carlton_Pin.png";
 
 const productImages = {
   beau_pin: BeauPin,
+  carlton_pin: CarltonPin,
 };
 
 export default productImages;
